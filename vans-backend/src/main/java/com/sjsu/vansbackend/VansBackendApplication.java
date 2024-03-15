36b50eb530a2220412b1b91e422ccfa26eb97f28
P. Soumith Reddy <soumithreddy.podduturi@sjsu.edu>
2024-03-14 18:54:19 -0700

    Initial Project Setup

---
+++ vans-backend/src/main/java/com/sjsu/vansbackend/VansBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sjsu.vansbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VansBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VansBackendApplication.class, args);
+    }
+
+}
---
+++ vans-backend/src/test/java/com/sjsu/vansbackend/VansBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjsu.vansbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VansBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
